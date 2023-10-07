5c263da832aa1d62d0333b9423e59226ce30890f
Ilya <mr.king101@ukr.net>
2023-10-08 00:04:47 +0300

    dop js_code

--- script.js
+++ script.js
@@ -13,3 +13,12 @@ Counter('.btn2','.spn2')
 Counter('.btn3','.spn3')
 Counter('.btn4','.spn4')
 Counter('.btn5','.spn5')
+
+// let count = [0,0,0,0,0,0]
+
+// for (let i = 1; i<=5; i++) {
+//   document.querySelector(".btn"+ String(i)).addEventListener('click', () =>{
+//     count[i] ++
+//     document.querySelector(".spn"+ String(i)).textContent = count[i];
+//   })
+// };
