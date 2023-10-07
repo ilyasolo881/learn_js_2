99d7e739bb27606e71c2b7872d1b8c8eaea532ba
Ilya <mr.king101@ukr.net>
2023-10-07 23:32:04 +0300

    optimisation

--- script.js
+++ script.js
@@ -1,45 +1,15 @@
-// const counters = document.querySelectorAll(".flex-counter");
+function Counter(btn, spn){
+	const button = document.querySelector(btn)
+	const span = document.querySelector(spn)
+	let count = 0
 
-// if (counters) {
-//   counters.forEach(counter => {
-//     counter.addEventListener('click', e => {
-//       const target = e.target;
-
-//       if (target.closest("flex-counter__button")) {
-//         let value = parseInt(target.closest(".flex-counter").querySelector("input").value);
-        
-//         if (target.classList.contains("flex-counter__button")){
-//           value ++;
-//         }
-
-//         target.closest(".flex-counter").querySelector("input").value = value
-//       }
-//     })
-//   }  
-//   )
-// }
-
-const counter = document.querySelectorAll(".counter")
-
-if (target)
-// if (counters) {
-// 	counters.forEach(counter => {
-// 		counter.addEventListener('click', e => {
-// 			const target = e.target;
-
-// 			if (target.closest('.counter__button')) {
-// 				if (target.closest('.counter').querySelector('h1').value == '' && (target.classList.contains('counter__button'))) {
-// 					target.closest('.counter').querySelector('h1').value = 0;
-// 				}
-
-// 				let value = parseInt(target.closest('.counter').querySelector('h1').value);
-
-// 				if (target.classList.contains('counter__button')) {
-// 					value++;
-//         }
-
-// 				target.closest('.counter').querySelector('h1').value = value;
-// 			}
-// 		})
-// 	})	
-// }
+	button.addEventListener('click', () =>{
+		count ++
+		span.textContent = count;
+	})
+}
+Counter('.btn1','.spn1')
+Counter('.btn2','.spn2')
+Counter('.btn3','.spn3')
+Counter('.btn4','.spn4')
+Counter('.btn5','.spn5')
