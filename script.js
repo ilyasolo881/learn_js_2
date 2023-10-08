fab265c0e0663370541c271db77acec99ddc8df9
Ilya <mr.king101@ukr.net>
2023-10-08 23:31:24 +0300

    +-done

--- script.js
+++ script.js
@@ -1,40 +1,55 @@
-/* Устанавливаем стартовый индекс слайда по умолчанию: */
 let slideIndex = 1;
-/* Вызываем функцию, которая реализована ниже: */
 showSlides(slideIndex);
 
-/* Увеличиваем индекс на 1 — показываем следующий слайд: */
-function nextSlide() {
+function nextSlide(){
     showSlides(slideIndex += 1);
+    noEndless();
 }
-
-/* Уменьшаем индекс на 1 — показываем предыдущий слайд: */
-function previousSlide() {
-    showSlides(slideIndex -= 1);  
+function previousSlide(){
+    showSlides(slideIndex -= 1);
+    noEndless();
 }
-
-/* Устанавливаем текущий слайд: */
-function currentSlide(n) {
+function currentSlide(n){
     showSlides(slideIndex = n);
 }
-
-/* Функция перелистывания: */
-function showSlides(n) {
-    /* Обращаемся к элементам с названием класса "item", то есть к картинкам: */
-    let slides = document.getElementsByClassName("item");
-    
-    /* Проверяем количество слайдов: */
+function showSlides(n){
+    let slides = document.querySelectorAll(".item");
     if (n > slides.length) {
       slideIndex = 1
     }
     if (n < 1) {
         slideIndex = slides.length
     }
-  
-    /* Проходим по каждому слайду в цикле for: */
     for (let slide of slides) {
         slide.style.display = "none";
     }
-    /* Делаем элемент блочным: */
     slides[slideIndex - 1].style.display = "block";    
+}
+
+function noEndless(){
+    const check = document.querySelector(".noEndless");
+    const button1 = document.querySelector(".previous");
+    const button2 = document.querySelector(".next");
+    if (check.checked) {
+        if (slideIndex === 1) {
+            button1.style.pointerEvents = "none";
+        }
+        else{
+            button1.style.pointerEvents = "visiblePainted";
+        }
+        if(slideIndex === 3){
+            button2.style.pointerEvents = "none";
+        }
+        else{
+            button2.style.pointerEvents = "visiblePainted";
+        }
+    }
+}
+
+function autoplay(){
+    const button = document.querySelector(".next");
+    const check = document.querySelector(".autoplay");
+    if (check.checked) {
+        setInterval (() => button.click(), 2000)
+    }
 }
