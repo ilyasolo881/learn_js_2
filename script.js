49f8d367ffe21b0fe38e0311ece03501f06c98e4
Ilya <mr.king101@ukr.net>
2023-10-25 12:31:48 +0300

    massage

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-function Counter(btn, spn){
-	const button = document.querySelector(btn)
-	const span = document.querySelector(spn)
-	let count = 0
+function Counter(btn, spn) {
+  const button = document.querySelector(btn);
+  const span = document.querySelector(spn);
+  let count = 0;
 
-	button.addEventListener('click', () =>{
-		count ++
-		span.textContent = count;
-	})
+  button.addEventListener("click", () => {
+    count++;
+    span.textContent = count;
+  });
 }
-Counter('.btn1','.spn1')
-Counter('.btn2','.spn2')
-Counter('.btn3','.spn3')
-Counter('.btn4','.spn4')
-Counter('.btn5','.spn5')
+Counter(".btn1", ".spn1");
+Counter(".btn2", ".spn2");
+Counter(".btn3", ".spn3");
+Counter(".btn4", ".spn4");
+Counter(".btn5", ".spn5");
 
 // let count = [0,0,0,0,0,0]
 
